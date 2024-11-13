3bb0a3ccec64f4d5043b3a64683cdabc1d1f4a9c
Youssef Hesham <ucefhesham@gmail.com>
2024-11-13 20:49:48 +0200

    Some Changes

--- Main/app.js
+++ Main/app.js
@@ -10,4 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = app;
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Express' }); 
+});
+
+app.listen(3000);
