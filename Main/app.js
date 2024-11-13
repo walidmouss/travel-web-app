076d492019651316263a0410703a384f0eaff413
Youssef Hesham <ucefhesham@gmail.com>
2024-11-13 22:34:00 +0200

    Added MongoDB

--- Main/app.js
+++ Main/app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.render('index', { title: 'Express' }); 
+  res.render('login'); 
 });
 
 app.listen(3000);
