34f457b82c3c4653a1d61ef437fafe52c9e0d427
Hatem Soliman <hatemthedev@gmail.com>
2024-12-19 22:17:25 +0200

    url implemented

--- constants/index.js
+++ constants/index.js
@@ -61,7 +61,7 @@ const locations = [
     price: '$1,139/mo',
     background: '/background_annapurna.jpg',
     description: 'Annapurna is a breathtaking massif located in the Himalayas of north-central Nepal, boasting stunning peaks, including one towering over 8,000 meters. It is a haven for trekkers and mountaineers, offering dramatic landscapes, rich biodiversity, and a deep connection to Nepali culture and traditions. Annapurna Circuit and Base Camp treks are among the most sought-after adventures in the world, blending natural beauty with an unforgettable cultural experience.',
-    video: '/annapurna.mp4'
+    video: 'https://www.youtube.com/embed/KHvOOuecnqA?si=Xwuf4EmPoGLIW7ec'
   },
   {
     name: 'Bali',
@@ -70,7 +70,7 @@ const locations = [
     price: '$1,139/mo',
     background: '/background_bali.jpg',
     description: 'Bali, Indonesia\'s enchanting tropical island, is a paradise known for its lush forested volcanic mountains, breathtaking waterfalls, terraced rice paddies, and serene beaches fringed by coral reefs. Whether it\'s the vibrant nightlife of Kuta, the tranquil retreats of Ubud, or the sacred temples like Tanah Lot and Uluwatu, Bali offers a unique blend of relaxation and adventure. Visitors also enjoy its vibrant art scene, world-class surfing spots, and deeply spiritual Hindu culture.',
-    video: '/bali.mp4'
+    video: 'https://www.youtube.com/embed/VoWepqBuw3Y?si=Dt0dkQKWjcQ--K5B'
   },
   {
     name: 'Inca',
@@ -79,7 +79,7 @@ const locations = [
     price: '$1,139/mo',
     background: '/background_inca.jpg',
     description: 'The Inca Trail is a legendary hiking route that meanders through the Andes, culminating at the iconic Machu Picchu, the ancient Incan citadel in Peru. Along the trail, hikers encounter awe-inspiring mountain vistas, lush cloud forests, and well-preserved archaeological sites that provide a glimpse into the advanced engineering and culture of the Inca Empire. The trek is a journey of both physical challenge and profound historical discovery, making it a bucket-list experience.',
-    video: '/inca.mp4'
+    video: 'https://www.youtube.com/embed/eFaCI0GcvaE?si=C7_HcbXUMfMwiMiy'
   },
   {
     name: 'Paris',
@@ -88,7 +88,7 @@ const locations = [
     price: '$1,139/mo',
     background: '/background_paris.jpg',
     description: 'Paris, the enchanting capital of France, is a global hub of art, fashion, and gastronomy. Known as the "City of Light," it dazzles with its iconic landmarks such as the Eiffel Tower, the Louvre Museum, and Notre-Dame Cathedral. Stroll through its charming cobblestone streets, indulge in world-renowned French cuisine, or relax in the elegant gardens of Luxembourg or Tuileries. Paris is a timeless city that seamlessly blends historical charm with modern sophistication.',
-    video: 'https://www.youtube.com/embed/P3RWXEvaiwo?si=KQ8yW48PbGVB1_KO'
+    video: 'https://www.youtube.com/embed/REDVbTQxMXo?si=boNWonUIy0W7AwWZ'
   },
   {
     name: 'Rome',
@@ -97,7 +97,7 @@ const locations = [
     price: '$1,139/mo',
     background: '/background_rome.jpg',
     description: 'Rome, the eternal capital of Italy, boasts a history spanning nearly 3,000 years, making it one of the most culturally and historically significant cities in the world. The city is an open-air museum, home to iconic sites like the Colosseum, the Roman Forum, the Pantheon, and Vatican City. Beyond its ancient monuments, Rome offers a vibrant street life, exquisite Italian cuisine, and charming piazzas that reflect its warm, lively spirit. Experience a city where every corner tells a story.',
-    video: '/rome.mp4'
+    video: 'https://www.youtube.com/embed/2x16HZIzsKQ?si=nlXYbh7KqgLiOyyD'
   },
   {
     name: 'Santorini',
@@ -106,7 +106,7 @@ const locations = [
     price: '$2,875/mo',
     background: '/background3.jpg',
     description: 'Santorini, a gem of the Aegean Sea, captivates visitors with its stunning turquoise waters, whitewashed buildings with blue-domed roofs, and dramatic volcanic cliffs. Famous for its breathtaking sunsets over the caldera, Santorini offers a wealth of activities, from wine-tasting tours at local vineyards to exploring ancient archaeological sites like Akrotiri. Its idyllic setting, coupled with authentic Greek cuisine and proximity to other islands for hopping adventures, makes it one of the most popular destinations for travelers seeking romance and relaxation.',
-    video: 'https://www.youtube.com/embed/O3U7vAVUca0?si=KyYRGD8XIGS6N3si'
+    video: 'https://www.youtube.com/embed/_YWVeY8taTA?si=j2jdB8RSKA-aBua'
   }
 ];
 
